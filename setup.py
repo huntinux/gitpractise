4374c5a983be5d476cea579da533ca365da32fcc
hongjin.cao <hongjin.cao@qunar.com>
2014-02-21 20:10:08 +0800

    add setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,13 @@
-import os
-from setuptools import setup
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
+from setuptools import setup, find_packages
 setup(
-    name = "an_example_project",
-    version = "0.0.4",
+    name = "test",
+    version = "0.1",
     author = "hongjin.cao",
-    author_email = "hognjin.cao@qunar.com",
-    description = ("An demonstration of how to create, document, and publish "
-                                   "a project"),
-    license = "BSD",
-    keywords = "example documentation tutorial",
+    author = "hongjin.cao@qunar.com",
     url = "http://gitlab.corp.qunar.com/hongjin.cao/newproject/tree/master",
-    packages=['an_example_project'],
-    long_description=read('README.md'),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: BSD License",
-    ],
+    packages=find_packages(),
+    install_requires = ['requests']
 )
 
-# add some trash
+# 引用的package写在install_requires中。
+
